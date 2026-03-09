3adaf59c90405ec55c7df5b3ab55bb9b51d4e18e
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-09 19:44:09 -0300

    Add template files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sembastandalone.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public String Index()
+    {
+        return "index";
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public String Error()
+    {
+        return "error";
+    }
+}

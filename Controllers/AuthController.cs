1d417099544b79160a4d1a4bdfeaad6b9783529f
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-10 11:58:46 -0300

    Add missing auth routes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -20,4 +20,28 @@ public class AuthController : Controller {
 
         return RequestSerializer.Serialize(res);
     }
+
+    [Route("/auth/sign_in")]
+    public async Task<IActionResult> Auth_SignIn() {
+        var req = await RequestSerializer.Deserialize<AuthSignInRequest>(Request);
+
+        var res = AuthModel.Auth_SignIn(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/auth/nonce")]
+    public IActionResult Auth_Nonce() {
+        var res = AuthModel.Auth_Nonce();
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
 }

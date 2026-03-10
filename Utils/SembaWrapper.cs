c5e6b2df8bddcf176dbfc49f7eb29b118cd14ed1
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-10 11:17:35 -0300

    Relax JSON serialization options

--- Utils/SembaWrapper.cs
+++ Utils/SembaWrapper.cs
@@ -4,6 +4,10 @@ using System.Runtime.InteropServices;
 using System.Text.Json;
 
 public class SembaWrapper {
+    public static JsonSerializerOptions options = new JsonSerializerOptions(
+        JsonSerializerDefaults.Web
+    );
+
     public static void Init(string dbPath) {
         Semba.NimMain();
         var dbPathUtf8 = Marshal.StringToCoTaskMemUTF8(dbPath);
@@ -13,12 +17,23 @@ public class SembaWrapper {
 
     public static ResType? Call<ResType, ReqType>(string path, ReqType request) {
         var pathUtf8 = Marshal.StringToCoTaskMemUTF8(path);
-        var reqJsonUtf8 = Marshal.StringToCoTaskMemUTF8(JsonSerializer.Serialize(request));
+        var reqJsonUtf8 = Marshal.StringToCoTaskMemUTF8(JsonSerializer.Serialize(request, options));
         // FIXME: add a SembaFree in libsemba
         var resUtf8 = Semba.SembaCall(pathUtf8, reqJsonUtf8);
         Marshal.FreeCoTaskMem(pathUtf8);
         Marshal.FreeCoTaskMem(reqJsonUtf8);
         var resJson = Marshal.PtrToStringUTF8(resUtf8);
-        return (resJson != null) ? JsonSerializer.Deserialize<ResType>(resJson) : default(ResType);
+        return (resJson != null) ? JsonSerializer.Deserialize<ResType>(resJson, options) : default(ResType);
+    }
+
+    public static ResType? Call<ResType>(string path) {
+        var pathUtf8 = Marshal.StringToCoTaskMemUTF8(path);
+        var reqJsonUtf8 = Marshal.StringToCoTaskMemUTF8("");
+        // FIXME: add a SembaFree in libsemba
+        var resUtf8 = Semba.SembaCall(pathUtf8, reqJsonUtf8);
+        Marshal.FreeCoTaskMem(pathUtf8);
+        Marshal.FreeCoTaskMem(reqJsonUtf8);
+        var resJson = Marshal.PtrToStringUTF8(resUtf8);
+        return (resJson != null) ? JsonSerializer.Deserialize<ResType>(resJson, options) : default(ResType);
     }
 }

38662f42eabbb955c4e7a3431b00699c401b1cf2
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-11 17:58:53 -0300

    Add dungeon endpoints

---
+++ Controllers/DungeonController.cs
@@ -0,0 +1,76 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+using sembastandalone.Utils;
+using sembastandalone.Models;
+using Neon.Model.Api.Rpc;
+
+namespace sembastandalone.Controllers;
+
+public class DungeonController : Controller {
+    [Route("/dungeon/battle_start")]
+    public async Task<IActionResult> Dungeon_BattleStart() {
+        var req = await RequestSerializer.Deserialize<DungeonBattleStartRequest>(Request);
+
+        var res = DungeonModel.Dungeon_BattleStart(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/dungeon/entry")]
+    public async Task<IActionResult> Dungeon_Entry() {
+        var req = await RequestSerializer.Deserialize<DungeonEntryRequest>(Request);
+
+        var res = DungeonModel.Dungeon_Entry(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/dungeon/finish")]
+    public async Task<IActionResult> Dungeon_Finish() {
+        var req = await RequestSerializer.Deserialize<DungeonFinishRequest>(Request);
+
+        var res = DungeonModel.Dungeon_Finish(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/dungeon/resume")]
+    public async Task<IActionResult> Dungeon_Resume() {
+        var req = await RequestSerializer.Deserialize<DungeonResumeRequest>(Request);
+
+        var res = DungeonModel.Dungeon_Resume(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/dungeon/start")]
+    public async Task<IActionResult> Dungeon_Start() {
+        var req = await RequestSerializer.Deserialize<DungeonStartRequest>(Request);
+
+        var res = DungeonModel.Dungeon_Start(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+}

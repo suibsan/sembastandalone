1b2ee9bbe36337252fbda7d9c39ce30c21f15783
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-11 18:47:35 -0300

    Add missing user endpoints

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,9 +7,31 @@ using Neon.Model.Api.Rpc;
 namespace sembastandalone.Controllers;
 
 public class UserController : Controller {
+    [Route("/user/cross_date")]
+    public IActionResult User_CrossDate() {
+        var res = UserModel.User_CrossDate();
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
     [Route("/user/log_in")]
     public IActionResult User_LogIn() {
         var res = UserModel.User_LogIn();
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/user/notification")]
+    public IActionResult User_Notification() {
+        var res = UserModel.User_Notification();
 
         if (res == null) {
             return StatusCode(500);
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -6,7 +6,15 @@ using sembastandalone.Utils;
 using System.Text.Json; 
 
 public class UserModel {
+    public static UserCrossDateResponse? User_CrossDate() {
+        return UserCrossDateResponse.Parser.ParseJson(SembaWrapper.Call("/user/cross_date", ""));
+    }
+
     public static UserLogInResponse? User_LogIn() {
         return UserLogInResponse.Parser.ParseJson(SembaWrapper.Call("/user/log_in", ""));
+    }
+
+    public static ChangedResourcesResponse? User_Notification() {
+        return ChangedResourcesResponse.Parser.ParseJson(SembaWrapper.Call("/user/notification", ""));
     }
 }

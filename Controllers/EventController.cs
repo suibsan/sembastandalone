e74f329de5798983c0c774fc43bd8edd3b4d880b
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-11 18:06:05 -0300

    Add event endpoints

---
+++ Controllers/EventController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+using sembastandalone.Utils;
+using sembastandalone.Models;
+using Neon.Model.Api.Rpc;
+
+namespace sembastandalone.Controllers;
+
+public class EventController : Controller {
+    [Route("/event/finish_node")]
+    public async Task<IActionResult> Event_FinishNode() {
+        var req = await RequestSerializer.Deserialize<EventFinishNodeRequest>(Request);
+
+        var res = EventModel.Event_FinishNode(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/event/list_node")]
+    public IActionResult Event_ListNode() {
+        var res = EventModel.Event_ListNode();
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+}

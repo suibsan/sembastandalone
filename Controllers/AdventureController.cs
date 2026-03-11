67bcfcc28b9c48b65937c7dd8e6fe6503831bd49
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-11 16:38:53 -0300

    Add adventure endpoints

---
+++ Controllers/AdventureController.cs
@@ -0,0 +1,126 @@
+using Microsoft.AspNetCore.Mvc;
+
+using sembastandalone.Utils;
+using sembastandalone.Models;
+using Neon.Model.Api.Rpc;
+
+namespace sembastandalone.Controllers;
+
+public class AdventureController : Controller {
+    [Route("/adventure/access_warp_point")]
+    public async Task<IActionResult> Adventure_AccessWarpPoint() {
+        var req = await RequestSerializer.Deserialize<AdventureAccessWarpPointRequest>(Request);
+
+        var res = AdventureModel.Adventure_AccessWarpPoint(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/acquire_area_item")]
+    public async Task<IActionResult> Adventure_AcquireAreaItem() {
+        var req = await RequestSerializer.Deserialize<AdventureAcquireAreaItemRequest>(Request);
+
+        var res = AdventureModel.Adventure_AcquireAreaItem(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/area_object")]
+    public async Task<IActionResult> Adventure_AreaObject() {
+        var req = await RequestSerializer.Deserialize<AdventureAreaObjectRequest>(Request);
+
+        var res = AdventureModel.Adventure_AreaObject(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/hospital")]
+    public async Task<IActionResult> Adventure_Hospital() {
+        var req = await RequestSerializer.Deserialize<AdventureHospitalRequest>(Request);
+
+        var res = AdventureModel.Adventure_Hospital(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/move_to_area")]
+    public async Task<IActionResult> Adventure_MoveToArea() {
+        var req = await RequestSerializer.Deserialize<AdventureMoveToAreaRequest>(Request);
+
+        var res = AdventureModel.Adventure_MoveToArea(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/read_sequence")]
+    public async Task<IActionResult> Adventure_ReadSequence() {
+        var req = await RequestSerializer.Deserialize<AdventureReadSequenceRequest>(Request);
+
+        var res = AdventureModel.Adventure_ReadSequence(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/release_event_lift")]
+    public async Task<IActionResult> Adventure_ReleaseEventLift() {
+        var req = await RequestSerializer.Deserialize<AdventureReleaseEventLiftRequest>(Request);
+
+        var res = AdventureModel.Adventure_ReleaseEventLift(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/update_character_status")]
+    public async Task<IActionResult> Adventure_UpdateCharacterStatus() {
+        var req = await RequestSerializer.Deserialize<AdventureUpdateCharacterStatusRequest>(Request);
+
+        var res = AdventureModel.Adventure_UpdateCharacterStatus(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+
+    [Route("/adventure/warp_area_locator")]
+    public async Task<IActionResult> Adventure_WarpAreaLocator() {
+        var req = await RequestSerializer.Deserialize<AdventureWarpAreaLocatorRequest>(Request);
+
+        var res = AdventureModel.Adventure_WarpAreaLocator(req);
+
+        if (res == null) {
+            return StatusCode(500);
+        }
+
+        return RequestSerializer.Serialize(res);
+    }
+}

d9f8d653c0fb9d2848010e00c293b6f330066272
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-14 01:38:32 -0300

    Add generator

---
+++ scripts/gen_controllers_and_models.py
@@ -0,0 +1,72 @@
+from argparse import ArgumentParser
+from collections import defaultdict
+from pathlib import Path
+import re
+
+from generator import gen_controller_and_model
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("signatures_csv")
+    parser.add_argument("controllers_out", type=Path)
+    parser.add_argument("models_out", type=Path)
+    args = parser.parse_args()
+
+    with open(args.signatures_csv, "r", encoding="utf-8") as f:
+        lines = re.findall(r"([^\n]+)", f.read())
+        signatures = map(lambda x: x.split(","), lines)
+        signatures = list(signatures)
+    
+    controllers = defaultdict(list)
+    models = defaultdict(list)
+
+    for method_name, req_type, res_type in signatures:
+        class_ = method_name.split("_")[0]
+        _, controller, model = gen_controller_and_model(method_name, req_type, res_type)
+
+        controllers[class_].append(controller)
+        models[class_].append(model)
+
+    args.controllers_out.mkdir(exist_ok=True)
+    args.models_out.mkdir(exist_ok=True)
+
+    for class_, controller_methods in controllers.items():
+        with open(args.controllers_out/f"{class_}Controller.cs", "w", encoding="utf-8") as f:
+            write_controller(f, class_, controller_methods)
+        
+        model_methods = models[class_]
+
+        with open(args.models_out/f"{class_}Model.cs", "w", encoding="utf-8") as f:
+            write_model(f, class_, model_methods)
+
+
+def write_controller(f, class_, controller_methods):
+    f.write(f"""
+using Microsoft.AspNetCore.Mvc;
+
+using sembastandalone.Utils;
+using sembastandalone.Models;
+using Neon.Model.Api.Rpc;
+
+namespace sembastandalone.Controllers;
+
+public class {class_}Controller : Controller {{
+{"\n".join(controller_methods)}
+}}
+""")
+
+def write_model(f, class_, model_methods):
+    f.write(f"""
+namespace sembastandalone.Models;
+
+using Neon.Model.Api.Rpc;
+using sembastandalone.Utils;
+
+public class {class_}Model {{
+{"\n".join(model_methods)}
+}}
+""")
+             
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/generator.py
@@ -0,0 +1,231 @@
+import re
+
+
+def get_route(class_, name):
+    return f"/{pascal_case_to_snake_case(class_)}/{pascal_case_to_snake_case(name)}"
+
+
+def pascal_case_to_snake_case(name):
+    return name[:1].lower() + re.sub(
+        r"[A-Z]", lambda x: "_" + x.group(0).lower(), name[1:]
+    )
+
+
+def gen_controller(route, class_, method_name, req_type, res_type):
+    if req_type != "None":
+        if res_type != "None":
+            return_val = "async Task<IActionResult>"
+        else:
+            return_val = "async Task"
+
+        req_assignment = f"var req = await RequestSerializer.Deserialize<{req_type}>(Request);"
+        req_arg = "req"
+    else:
+        if res_type != "None":
+            return_val = "IActionResult"
+        else:
+            return_val = "void"
+
+        req_assignment = "// no request"
+        req_arg = ""
+
+    if res_type != "None":
+        res_assignment = "var res = "
+        serialize_res = """if (res == null) {
+        return StatusCode(500);
+    }
+
+    return RequestSerializer.Serialize(res);
+"""
+    else:
+        res_assignment = ""
+        serialize_res = """// no response
+"""
+
+    return f"""
+[Route("{route}")]
+public {return_val} {method_name}() {{
+    {req_assignment}
+
+    {res_assignment}{class_}Model.{method_name}({req_arg});
+
+    {serialize_res}}}
+"""
+
+
+def gen_model(route, class_, method_name, req_type, res_type):
+    if req_type != "None":
+        req_log = f"""Console.WriteLine($"{method_name}: {{req}}");"""
+        req_param = f"{req_type} req"
+        req_tostring = "req.ToString()"
+    else:
+        req_log = "// no request"
+        req_param = ""
+        req_tostring = '""'
+
+    semba_wrapper_call = f"""SembaWrapper.Call("{route}", {req_tostring})"""
+
+    if res_type != "None":
+        return_type = f"{res_type}?"
+        return_parsejson = f"""return {res_type}.Parser.ParseJson(
+        {semba_wrapper_call}
+    )"""
+    else:
+        return_type = "void"
+        return_parsejson = semba_wrapper_call
+
+    return f"""
+public static {return_type} {method_name}({req_param}) {{
+    {req_log}
+    {return_parsejson};
+}}
+"""
+
+
+def gen_controller_and_model(method_name, req_type, res_type):
+    class_, name = method_name.split("_")
+
+    route = get_route(class_, name)
+    
+    controller = gen_controller(route, class_, method_name, req_type, res_type)
+    
+    model = gen_model(route, class_, method_name, req_type, res_type)
+    
+    return route, controller, model
+
+
+def test_signature_with_req_and_res():
+    route, controller, model = gen_controller_and_model(
+        "Adventure_AccessWarpPoint",
+        "AdventureAccessWarpPointRequest",
+        "AdventureAccessWarpPointResponse"
+    )
+
+    assert route == "/adventure/access_warp_point"
+
+    assert controller == """
+[Route("/adventure/access_warp_point")]
+public async Task<IActionResult> Adventure_AccessWarpPoint() {
+    var req = await RequestSerializer.Deserialize<AdventureAccessWarpPointRequest>(Request);
+
+    var res = AdventureModel.Adventure_AccessWarpPoint(req);
+
+    if (res == null) {
+        return StatusCode(500);
+    }
+
+    return RequestSerializer.Serialize(res);
+}
+"""
+
+    assert model == """
+public static AdventureAccessWarpPointResponse? Adventure_AccessWarpPoint(AdventureAccessWarpPointRequest req) {
+    Console.WriteLine($"Adventure_AccessWarpPoint: {req}");
+    return AdventureAccessWarpPointResponse.Parser.ParseJson(
+        SembaWrapper.Call("/adventure/access_warp_point", req.ToString())
+    );
+}
+"""
+
+
+def test_signature_without_req():
+    route, controller, model = gen_controller_and_model(
+        "Auth_Nonce",
+        "None",
+        "AuthNonceResponse"
+    )
+
+    assert route == "/auth/nonce"
+
+    assert controller == """
+[Route("/auth/nonce")]
+public IActionResult Auth_Nonce() {
+    // no request
+
+    var res = AuthModel.Auth_Nonce();
+
+    if (res == null) {
+        return StatusCode(500);
+    }
+
+    return RequestSerializer.Serialize(res);
+}
+"""
+
+    assert model == """
+public static AuthNonceResponse? Auth_Nonce() {
+    // no request
+    return AuthNonceResponse.Parser.ParseJson(
+        SembaWrapper.Call("/auth/nonce", "")
+    );
+}
+"""
+
+
+def test_signature_without_res():
+    route, controller, model = gen_controller_and_model(
+        "User_UpdateLanguage",
+        "UserUpdateLanguageRequest",
+        "None"
+    )
+
+    assert route == "/user/update_language"
+
+    assert controller == """
+[Route("/user/update_language")]
+public async Task User_UpdateLanguage() {
+    var req = await RequestSerializer.Deserialize<UserUpdateLanguageRequest>(Request);
+
+    UserModel.User_UpdateLanguage(req);
+
+    // no response
+}
+"""
+
+    assert model == """
+public static void User_UpdateLanguage(UserUpdateLanguageRequest req) {
+    Console.WriteLine($"User_UpdateLanguage: {req}");
+    SembaWrapper.Call("/user/update_language", req.ToString());
+}
+"""
+
+
+def test_signature_without_both():
+    route, controller, model = gen_controller_and_model(
+        "Xb_ForceRetire",
+        "None",
+        "None"
+    )
+
+    assert route == "/xb/force_retire"
+
+    assert controller == """
+[Route("/xb/force_retire")]
+public void Xb_ForceRetire() {
+    // no request
+
+    XbModel.Xb_ForceRetire();
+
+    // no response
+}
+"""
+
+    assert model == """
+public static void Xb_ForceRetire() {
+    // no request
+    SembaWrapper.Call("/xb/force_retire", "");
+}
+"""
+
+
+def test_generator():
+    test_signature_with_req_and_res()
+    test_signature_without_req()
+    test_signature_without_res()
+    test_signature_without_both()
+
+    print("OK")
+
+
+if __name__ == "__main__":
+    test_generator()
